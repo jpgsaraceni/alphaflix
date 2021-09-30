ea19d9904ab56861ee6d260edf05c5803ea3c919
jpgsaraceni <jpgomes@id.uff.br>
2021-09-29 23:39:07 -0300

    bug fix

--- backend/src/controllers/login.controller.js
+++ backend/src/controllers/login.controller.js
@@ -4,6 +4,8 @@ export default function login(req, res) {
     const { username, password } = req.body;
 
     if (username === 'teste' && password === 'teste') {
-		sign(username).then(token => res.send(token).catch(res.sendStatus(500)))
+		sign(username)
+			.then(token => res.send(token))
+			.catch(() => res.sendStatus(500))
 	} else res.sendStatus(401);
 }
